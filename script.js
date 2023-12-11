6299bac6add502d4085f1cdc4248f1be3fd4cb9a
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-11 20:55:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function mincost(arr)
 { 
 //write your code here
+	let pq=[];
+	for(let ele of arr){
+		pq.push(ele);
+	}
+	pq.sort(function(a,b){
+		return a-b;
+	});
+	let sum=0;
+	while(pq.length>1){
+		let a=pq.shift();
+		let b=pq.shift();
+		sum+=(a+b);
+		pq.push((a+b));
+		pq.sort(function(a,b) {
+			return a-b;
+		});
+	}
 // return the min cost
-  
+  return sum;
 }
 
 module.exports=mincost;
